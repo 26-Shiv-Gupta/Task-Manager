b1175efd37fd8b848885550b5223df7c2d833f5b
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-03-22 15:54:53 +0530

    TaskList

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from '../Common/Header'
 import TaskListNumber from '../Common/TaskListNumber'
+import TaskList from '../TaskList/TaskList'
 
 const EmployeeDashboard = () => {
     return (
@@ -8,6 +9,7 @@ const EmployeeDashboard = () => {
             <div className='h-screen p-10'>
                 <Header />
                 <TaskListNumber />
+                <TaskList />
             </div>
         </>
     )
