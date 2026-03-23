4ce71f2ee7be2ecd01a10f0336752cf5b0610891
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-03-23 14:31:49 +0530

    get and set local storage added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Login from './components/Auth/Login'
 import EmployeeDashboard from './components/Dashboard/EmployeeDashboard'
 import AdminDashboard from './components/Dashboard/AdminDashboard'
+import { getLocalStorage, setLocalStorage } from './utils/localStorage'
 
 function App() {
+
+  useEffect(() => {
+    // setLocalStorage();
+    getLocalStorage();
+  },[])
+  
 
   return (
     <>
--- src/utils/localStorage.jsx
+++ src/utils/localStorage.jsx
@@ -1,2 +1,11 @@
 import data from './database.json'
 
+export const setLocalStorage = () => {
+    localStorage.setItem("employees", JSON.stringify(data.employees));
+    localStorage.setItem("admin", JSON.stringify(data.admin));
+}
+
+export const getLocalStorage = () => {
+    const employees = JSON.parse(localStorage.getItem("employees"));
+    const admin = JSON.parse(localStorage.getItem("admin"));
+}
