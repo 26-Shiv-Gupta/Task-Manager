d278db4fd808e93e7a32584fbd92758c2c35ee66
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-03-24 16:28:39 +0530

    database updated

--- src/App.jsx
+++ src/App.jsx
@@ -24,6 +24,7 @@ function App() {
 
   return (
     <>
+      {user}
       {!user ? <Login handleLogin={handleLogin} /> : user === 'admin' ? <AdminDashboard /> : <EmployeeDashboard />}
     </>
   )
