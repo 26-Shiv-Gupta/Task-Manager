499c270582c1142fe1224eecc0a9514d514239ab
26-Shiv-Gupta <gedashiv9977@gmail.com>
2026-03-31 13:02:41 +0530

    updated AuthProvider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -8,14 +8,14 @@ const AuthProvider = ({ children }) => {
     const [userData, setUserData] = useState(null);
 
     useEffect(() => {
-        setLocalStorage();
+        // setLocalStorage();
         const {employees, admins} = getLocalStorage();
         setUserData({employees, admins});
     }, [])
     
     return (
         <div>
-            <AuthContext.Provider value={userData}>
+            <AuthContext.Provider value={{userData, setUserData}}>
                 {children}
             </AuthContext.Provider>
         </div>
